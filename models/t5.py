59375fe61cdf98e630f856a032bb3fd305873acf
Fernanda Kipper <nanda.kipper@gmail.com>
2023-10-15 12:42:39 -0300

    feat: add T5 file

---
+++ models/t5.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+model_name = "t5-small"
+
+def create_t5_tokenizer():
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    return tokenizer
+
+def create_t5_summarizer():
+    tokenizer = create_bart_tokenizer()
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    t5_summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
+    return t5_summarizer
