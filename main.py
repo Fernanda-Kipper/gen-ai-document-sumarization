8f3f9c2b0407b445d1158a50a7492b7ef28a4a30
Fernanda <nanda.kipper@gmail.com>
2023-11-12 15:06:23 -0300

    feat: testing models

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from simplify_docx import simplify
 
 from models.bart import create_bart_summarizer
 from models.mt5 import create_mt5_summarizer
+from models.t5 import create_t5_summarizer
+from models.pegasus import create_pegasus_summarizer
 
 def break_text_in_sequences(extracted_text,max_length_sequence):
     segments = []
     for text in extracted_text:
+        text = text.replace("\n", " ")
         for i in range(0, len(text), max_length_sequence):
             segment = text[i:i + max_length_sequence]
             segments.append(segment)
-
     return segments
 
 def extract_text_recursive(docx_json):
@@ -50,10 +52,30 @@ def extract_text_from_docx(filename):
 
     return extracted_text
 
-# raw_text = extract_text_from_pdf('once-upon-a-time-test.pdf')
+def concat_summaries(summaries_list):
+    final_summary = ""
+    for summary in summaries_list:
+        final_summary += summary['summary_text']
+    return final_summary
+
+def calculate_max_length_of_summary(text_list):
+    min_length = 1024
+    for text in text_list:
+        if(len(text) < min_length):
+            min_length = len(text)
+    return min_length
+
+raw_text = extract_text_from_pdf('transformers.pdf')
 raw_text = extract_text_from_docx('once-upon-a-time-test.docx')
 processed_text = break_text_in_sequences(raw_text, 1024)
-bart_summarizer = create_bart_summarizer()
-mt5_summarizer = create_mt5_summarizer()
+max_length = calculate_max_length_of_summary(processed_text)
+min_length = int(max_length / 2)
 
-print(mt5_summarizer(processed_text, max_length=130, min_length=30, do_sample=False))
+bart_summarizer = create_bart_summarizer()
+# mt5_summarizer = create_mt5_summarizer()
+t5_summarizer = create_t5_summarizer()
+pegasus_summarizer = create_pegasus_summarizer()
+
+# print(concat_summaries(mt5_summarizer(raw_text, max_length=130, min_length=30, do_sample=False)))
+# print(concat_summaries(bart_summarizer(processed_text, max_length=130, min_length=30, do_sample=False)))
+print(concat_summaries(t5_summarizer(processed_text,max_length=max_length, min_length=min_length, do_sample=False)))
