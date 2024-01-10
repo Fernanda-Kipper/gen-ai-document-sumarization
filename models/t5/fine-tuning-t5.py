37e67c2ebcea3ad48a3d157da31e4f8eb6135a45
Fernanda <nanda.kipper@gmail.com>
2024-01-10 08:56:59 -0300

    feat: initial t5 fine tuning logic

---
+++ models/t5/fine-tuning-t5.py
@@ -0,0 +1,69 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+import evaluate
+import numpy as np
+
+rouge = evaluate.load("rouge")
+
+model = "t5-small"
+prefix = "summarize: "
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+patent_texts = load_dataset("big_patent", codes=["f"], split='train')
+patent_texts = patent_texts.train_test_split(test_size=0.2)
+
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["description"]]
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+    labels = tokenizer(text_target=examples["abstract"], max_length=128, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    result = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+
+    return {k: round(v, 4) for k, v in result.items()}
+
+tokenized_patents = patent_texts.map(preprocess_function, batched=True)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model)
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="t5_summarizer_model",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=4,
+    predict_with_generate=True,
+    push_to_hub=True,
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_patents["train"],
+    eval_dataset=tokenized_patents["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
