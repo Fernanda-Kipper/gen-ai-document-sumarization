91ea2432331e4d8089771a27a924053804568a31
Fernanda <nanda.kipper@gmail.com>
2024-02-18 14:17:01 -0300

    fix: evaluate script

--- models/evaluate.py
+++ models/evaluate.py
@@ -4,19 +4,22 @@ import numpy as np
 import random
 import torch
 
-dataset = load_dataset("samsum")
+dataset = load_dataset("big_patent", "d")
 
 rouge = load_metric("rouge")
 
 model_names = {
+    "T5": "KipperDev/t5_summarizer_model",
+    "BART": "KipperDev/bart_summarizer_model",
+    "PEGASUS": "google/pegasus-xsum",
     "T5": "t5-small",
     "BART": "facebook/bart-base",
 }
 
 percentage = 0.1
-sample_size = int(len(dataset["test"]) * percentage)
-sampled_indices = random.sample(range(len(dataset["test"])), sample_size)
-sampled_dataset = dataset["test"].select(sampled_indices)
+sample_size = int(len(dataset["train"]) * percentage)
+sampled_indices = random.sample(range(len(dataset["train"])), sample_size)
+sampled_dataset = dataset["train"].select(sampled_indices)
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
@@ -30,18 +33,18 @@ def compute_metrics(eval_pred):
     final_result["gen_len"] = np.mean(prediction_lens)
     return final_result
 
-def evaluate_model(model_name, tokenizer, model, dataset):
+def evaluate_model(model, tokenizer, dataset):
     summaries = []
     references = []
 
     for example in dataset:
-        inputs = tokenizer(example["dialogue"], return_tensors="pt", max_length=1024, truncation=True)
+        inputs = tokenizer(example["description"], return_tensors="pt", max_length=512, truncation=True)  # Reduzi o tamanho máximo de entrada para 512
 
         summary_ids = model.generate(inputs["input_ids"], max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
         summaries.append(summary)
-        references.append(example["summary"])
+        references.append(example["abstract"])
 
     eval_pred = (torch.tensor(tokenizer(summaries, padding=True, truncation=True)["input_ids"]),
                  torch.tensor(tokenizer(references, padding=True, truncation=True)["input_ids"]))
@@ -52,5 +55,5 @@ def evaluate_model(model_name, tokenizer, model, dataset):
 for model_name, model_path in model_names.items():
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
-    result = evaluate_model(model_name, tokenizer, model, sampled_dataset)
+    result = evaluate_model(model, tokenizer, sampled_dataset)
     print(f"Resultados para {model_name}: {result}")
